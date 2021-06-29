10358758677a2b76d5f5b0bc50d57fffebb2e229
krishna-kusum <59028548+krishna-kusum@users.noreply.github.com>
2021-06-29 11:28:02 +0530

    Add files via upload

---
+++ Devsnest/THA13/THA13.js
@@ -0,0 +1,27 @@
+const input = document.querySelector(".input")
+const submit = document.querySelector(".submit")
+const article = document.querySelector("article")
+
+
+function fetchQuotes() {
+				fetch(`https:api.quotable.io/random/`)
+				.then(res => res.json())
+				.then(data =>{ 
+								getResponce(data)
+				})
+}
+
+function getResponce(data){
+			
+			//console.log(JSON.stringify(data))
+			
+			let quote = document.createElement("div");
+			quote.className = "quotes";
+			quote.innerHTML = `${data.content}`
+			article.append(quote)
+			
+}
+
+fetchQuotes()
+
+
