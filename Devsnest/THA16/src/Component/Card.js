7ae03f333d408cb17e814d5bc511b16bc099d012
krishna-kusum <59028548+krishna-kusum@users.noreply.github.com>
2021-07-08 10:02:17 +0530

    Add files via upload

---
+++ Devsnest/THA16/src/Component/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Card() {
+  return (
+  <div className="card">
+    <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEL3bW5fnilunTmdaJO2y8lFF97W0XOq3l_g&usqp=CAU" alt = "" />
+    <h3>Meme Card </h3>
+  </div>
+  );
+};
+
+export default Card
