5e876900700ab1adb6e11645590e5aa9e3333342
krishna-kusum <59028548+krishna-kusum@users.noreply.github.com>
2021-07-18 08:45:39 +0530

    Add files via upload

---
+++ Devsnest/THA21/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import Form from './Components/Form'
+import Data from './Components/Data'
+
+export default function App() {
+  return (
+    <div className = "App" >
+      <Data/>
+      {/* <Form /> */}
+      {/* <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p> */}
+    </div>
+  );
+}
