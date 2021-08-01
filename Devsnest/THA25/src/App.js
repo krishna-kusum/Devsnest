12e428e894639d05b9ef1a4d239f362d05c56a72
Krishna Modi <59028548+krishna-kusum@users.noreply.github.com>
2021-08-01 22:19:06 +0530

    Add files via upload

---
+++ Devsnest/THA25/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles/tailwind-pre-build.css";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return <Navbar />;
+}
---
+++ Devsnest/THA25/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="h-20 bg-indigo-300 text-gray-800">
+      <ul className="flex items-center justify-between h-full px-10">
+        <li> Hi</li>
+        <li> Welcome to </li>
+        <li> Tailwind Css </li>
+      </ul>
+    </nav>
+  );
+}
